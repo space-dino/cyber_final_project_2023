3cc5c6cb1f8b7faa668d635aa473a080e3a8ca04
Space Dinosaur <91723941+space-dino@users.noreply.github.com>
2023-10-27 11:50:46 +0300

    Add files via upload

--- cli2.py
+++ cli2.py
@@ -1,11 +1,20 @@
 import socket
 import threading
+from tkinter import *
+from PIL import Image, ImageTk
 
+# socket creation
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = '127.0.0.1'  # Replace with the server's IP
+host = '127.0.0.1'
 port = 9999
 client_socket.connect((host, port))
+
+# GUI initiation
+root = Tk()
+root.title("Video Stream")
+
+labels = []
 
 
 def send():
@@ -15,9 +24,24 @@ def send():
 
 def receive():
     while True:
-        m = client_socket.recv(32)
-        print(m.decode())
+        index = client_socket.recv(4).decode()
+        frame_length = int(client_socket.recv(8).decode())
+        frame = client_socket.recv(frame_length).decode()
+
+        # print(str(index) + " " + str(frame_length) + " " + str(frame))
+
+        GUI_frame(frame, int(index))
+
+
+def GUI_frame(frame, index):
+    if index >= len(labels):
+        label = Label(root)
+        labels.append(label)
+    labels[index]['text'] = str(frame)
+    labels[index].grid(column=index, row=0, sticky='new', pady=20, padx=20)
 
 
 threading.Thread(target=send).start()
 threading.Thread(target=receive).start()
+
+root.mainloop()
