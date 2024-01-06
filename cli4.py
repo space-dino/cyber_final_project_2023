d6545c374c9a16e83b4e7a67c6dbd398686fc2d5
Space Dinosaur <91723941+space-dino@users.noreply.github.com>
2024-01-06 14:26:04 +0200

    disconnection and audio

--- cli4.py
+++ cli4.py
@@ -4,11 +4,14 @@ from threading import Thread
 import protocol4
 import cv2
 from PIL import Image, ImageTk
+import pyshine as ps # pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.8.0-py3-none-any.whl
 
 
 class client:
     def __init__(self):
         # <editor-fold desc="Socket Setup">
+        self.vid = None
+        print("Client up")
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.host = '127.0.0.1'
@@ -23,7 +26,12 @@ class client:
         self.root = tk.Tk()
 
         self.close_button = tk.Button(self.root, text="Close", command=self.close_connection)
-        self.close_button.grid(row=0, column=0)
+        self.close_button.grid(row=0, column=1)
+
+        self.index_label = tk.Label(self.root, text="index")
+        self.index_label.grid(row=1, column=1)
+        self.fps_label = tk.Label(self.root, text="fps")
+        self.fps_label.grid(row=2, column=1)
 
         self.labels = []
 
@@ -32,34 +40,53 @@ class client:
         self.data = b''
         self.my_index = 6
 
+        self.up = True
+
         self.start()
 
     def close_connection(self):
+        self.up = False
         print("Closing Connection")
+        self.vid.release()
         self.client_socket.close()
         self.root.destroy()
 
     def send(self):
-        vid = cv2.VideoCapture(0)  # 0 for the default camera
+        # Video
+        self.vid = cv2.VideoCapture(0)  # 0 for the default camera
+        fps = self.vid.get(cv2.CAP_PROP_FPS)
 
-        vid.set(3, 100)
-        vid.set(4, 75)
+        self.vid.set(3, 400)
+        self.vid.set(4, 300)
 
-        while True:
-            img, frame = vid.read()
+        # Audio
+        audio_get, context = ps.audioCapture(mode='send')
 
-            # indexes here dont matter becuase server isnt reading them
-            protocol4.send_frame(self.client_socket, frame, 0, 0)
-            self.draw_GUI_frame(frame, self.my_index)
+        while self.up:
+            img, unflipped = self.vid.read()
+            vid_frame = cv2.flip(unflipped, 1)
+
+            aud_frame = audio_get.get()
+
+            # indexes here dont matter becuase server isnt reading them. flag does matter!
+            protocol4.send_frame(self.client_socket, vid_frame, 0, 0, 0)  # video
+            self.draw_GUI_frame(vid_frame, self.my_index, fps)
+
+            protocol4.send_frame(self.client_socket, aud_frame, 1, 0, 0)  # audio
 
     def receive(self):
-        while True:
-            frame, self.data, index, self.my_index = protocol4.receive_frame(self.client_socket, self.data)
+        audio_put, context = ps.audioCapture(mode='get')
+
+        while self.up:
+            frame, self.data, flag, index, self.my_index = protocol4.receive_frame(self.client_socket, self.data)
 
             if bytes(frame) != b'' and index != self.my_index:
-                self.draw_GUI_frame(frame, int(index))
+                if flag == 0:  # video
+                    self.draw_GUI_frame(frame, int(index))
+                if flag == 1:  # audio
+                    audio_put.put(frame)
 
-    def draw_GUI_frame(self, frame, index):
+    def draw_GUI_frame(self, frame, index, fps):
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = Image.fromarray(frame)
         frame = ImageTk.PhotoImage(image=frame)
@@ -72,6 +99,9 @@ class client:
 
         self.labels[index].config(image=frame)
         self.labels[index].image = frame
+
+        self.index_label.config(text="client " + str(index))
+        self.fps_label.config(text=str(fps) + "fps")
 
         self.root.update()
 
--- ser4.py
+++ ser4.py
@@ -36,20 +36,52 @@ def accept_connections():
         clients.append(con)
         print("GOT CONNECTION FROM: " + str(addr[0]) + ":" + str(addr[1]) + "\nindex = " + str(cli_index) + "\n")
 
+        print_clients()
+
         Thread(target=send_to_client, args=(con,)).start()
         Thread(target=receive_from_client, args=(con,)).start()
 
 
 def send_to_client(con: connection):
-    while True:
+    b = True
+
+    while b:
         for i in range(len(clients)):
-            protocol4.send_frame(con.soc, con.frame, i, con.index)
+            try:
+                protocol4.send_frame(con.soc, con.frame, 0, i, con.index)
+            except ConnectionResetError:
+                b = False
 
 
 def receive_from_client(con: connection):
     while True:
-        # indexes here are obsolete and passed as a null value
-        con.frame, con.data, *_ = protocol4.receive_frame(con.soc, con.data)
+        try:
+            # indexes here are obsolete and passed as a null value
+            a, con.data, flag, *_ = protocol4.receive_frame(con.soc, con.data)
+
+            if flag == 0:
+                con.frame = a
+
+        except ConnectionResetError:
+            remove_client(con)
+
+
+def remove_client(con: connection):
+    for i in clients:
+        if i.index == con.index:
+            print("removing " + str(i))
+            clients.remove(i)
+
+            for o in range(0, len(clients)):
+                if clients[o] is None:
+                    if o < len(clients) - 1:
+                        clients[o].index = clients[o].index - 1
+    print_clients()
+
+
+def print_clients():
+    for i in clients:
+        print(i.index)
 
 
 accept_connections()
